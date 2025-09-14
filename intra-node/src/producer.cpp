280ae12bbd262234a4376ea79262d8548c60220c
wael2000 <waelibrahim2000@hotmail.com>
2025-09-14 22:19:22 +0200

    Done

--- intra-node/src/producer.cpp
+++ intra-node/src/producer.cpp
@@ -8,14 +8,14 @@
 const char *shm_name = "/my_shm";
 const size_t SIZE = 4096;
 
-void print_namespaces() {
-    std::cout << "=== Namespace Info ===" << std::endl;
-    for (const auto& entry : std::filesystem::directory_iterator("/proc/self/ns")) {
-        std::cout << entry.path().filename() << " -> " 
-                  << std::filesystem::read_symlink(entry.path()) << std::endl;
+std::string get_namespace_id(const std::string& ns_type) {
+    std::string ns_path = "/proc/self/ns/" + ns_type;
+    try {
+        auto link = std::filesystem::read_symlink(ns_path);
+        return link.string();  // e.g. "ipc:[4026531839]"
+    } catch (const std::exception& e) {
+        return "Error: " + std::string(e.what());
     }
-    std::cout << "======================" << std::endl;
-    
 }
 
 int main() {
@@ -38,7 +38,11 @@ int main() {
 
     // Write message
     const char *message = "Hello from Writer (Container 1)!";
-    std::memcpy(ptr, message, strlen(message) + 1);
+    std::string full_message = std::string(message) + " | PID=" + get_namespace_id("pid");
+
+    std::memcpy(ptr, full_message.c_str(), full_message.size() + 1);
+
+    // std::memcpy(ptr, message, strlen(message) + 1);
 
     print_namespaces();
     std::cout << "Writer wrote: " << message << std::endl;
